82f5ed6092ba85c81fc34c0b3c21de4a11459e2f
belalhosen <mdbelalhosen6234@gmail.com>
2022-11-24 15:53:49 +0600

    add first name and last name on dashboard

--- backend_files/dashboard.php
+++ backend_files/dashboard.php
@@ -2,7 +2,7 @@
 include('backend_slicePart_inc/header.php'); //include dashboard heading part
 ?>
 
-<h1 class="h3 mb-4 text-gray-800">Welcome To Dashboard</h1>
+<h1 class="h3 mb-4 text-gray-800">Welcome To Dashboard <?=ucwords($_SESSION['auth']['fName']) . ' ' . ucwords($_SESSION['auth']['lName'])?></h1>
 
 
 
