190f5292531f099c8738ca83101d9c9c457721dc
belalhosen <mdbelalhosen6234@gmail.com>
2022-11-29 17:03:19 +0600

    some changes and many fuctionality add

--- controller/banner_add.php
+++ controller/banner_add.php
@@ -5,17 +5,48 @@ if (isset($_POST['submit'])) {
   //* some action
 
   //*assigning all request
-  $banner_img = $_POST['banner_img'];
+
   $banner_title = $_POST['banner_title'];
   $banner_des = $_POST['banner_des'];
   $video_link = $_POST['video_link'];
+  $banner_img = $_FILES['banner_img'];
+  $extension = pathinfo($banner_img['name'])['extension'];
+  $ecepted_files = ['png', 'jpg', 'svg', 'jpeg'];
+  $found_img =  in_array($extension, $ecepted_files);
+  // print_r($extention);
+  // exit;
 
-  $query = "INSERT INTO add_banner_part(img_banner, banner_title, banner_des, video_link) VALUES ('$banner_img','$banner_title','$banner_des','$video_link')";
-  $exe = mysqli_query($conn, $query);
-  if ($exe) {
-    header("location: ../backend_files/add_banner.php");
-    $_SESSION['success'] = "Your Banner Added Succesfully !";
+  //* validation
+  $errors = [];
+  if (empty($banner_title)) {
+    $errors['banner_title_error'] = "Insert  Banner Title !";
   }
+  if (empty($banner_des)) {
+    $errors['banner_des_error'] = "Insert  Banner Description !";
+  }
+  if (empty($video_link)) {
+    $errors['video_link_error'] = "Insert  Video Link !";
+  }
+  if ($banner_img['size'] == 0) {
+    $errors['banner_img_error'] = "Insert  A Banner Image !";
+  } elseif ($found_img == false) {
+    $errors['banner_img_error'] = "Insert  A Proper Banner Image !";
+  }
+
+  if (count($errors) > 0) {
+    //*error foud
+    $_SESSION['errors'] = $errors;
+    // print_r($errors);
+    header("location: ../backend_files/add_banner.php");
+  } else {
+    //* no errors
+  }
+  // $query = "INSERT INTO add_banner_part(img_banner, banner_title, banner_des, video_link) VALUES ('$banner_img','$banner_title','$banner_des','$video_link')";
+  // $exe = mysqli_query($conn, $query);
+  // if ($exe) {
+  //   header("location: ../backend_files/add_banner.php");
+  //   $_SESSION['success'] = " Banner Added Succesfully !";
+  // }
 } else {
   //*fill the form
 }
